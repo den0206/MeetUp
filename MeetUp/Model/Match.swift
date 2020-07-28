4a132929f1c98f979f008734ba464d239c3a4e10
den0206 <mac.pop@mineo.jp>
2020-07-28 22:45:11 +0900

    init match model

---
+++ MeetUp/Model/Match.swift
@@ -0,0 +1,27 @@
+//
+//  Match.swift
+//  MeetUp
+//
+//  Created by 酒井ゆうき on 2020/07/28.
+//  Copyright © 2020 Yuuki sakai. All rights reserved.
+//
+
+import Foundation
+
+struct Match {
+    
+    let id : String
+    let memberIDs: [String]
+    let date : Date
+    
+    var dictionary : [String : Any] {
+        return [kOBJECTId : id,
+                kMEMBERIDS : memberIDs,
+                kDATE : date]
+    }
+    
+    func savrTofireStore() {
+        FirebaseReference(reference: .Like).document(self.id).setData(self.dictionary)
+    }
+    
+}
